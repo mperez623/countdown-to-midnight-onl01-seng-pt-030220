fe64278f6097fc1287bd81006a4343cc4af9df9d
Melissa Perez <melissaperezg.96@gmail.com>
2020-03-13 04:42:11 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
-#write your code here
+def countdown(number)
+  while number > 0 
+  puts "#{number} SECOND(S)!"
+  number -= 1
+end
+"HAPPY NEW YEAR!"
+end 
 
-def countdown
+def countdown_with_sleep(count)
+  while count > 0 
+    count -=1
+    sleep (1)
+  end 
+    puts "0 left. HAPPY NEW YEAR!"
 end
